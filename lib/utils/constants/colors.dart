4e7e62aa423c2eaa3973a591cbdd56f5676326ee
Dev. Smith <husseinshakir81@gmail.com>
2023-11-26 23:19:06 +0300

    feat(setup): setup app colors class

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class EColors {
+  EColors._();
+
+  // App Basic Colors
+  static const primary = Color(0xFF6A87E3);
+  static const secondary = Color(0xFFFFE24B);
+  static const accent = Color(0xFFb0c7ff);
+
+  // LinearGradient
+  static Gradient linerGradient = const LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [
+      Color(0xffff9a9e),
+      Color(0xfffad0c4),
+      Color(0xfffad0c4),
+    ],
+  );
+
+  // text colors
+  static const textPrimary = Color(0xFF333333);
+  static const textSecondary = Color(0xFF6C7570);
+  static const textWhite = Colors.white;
+
+  // Background Colors
+  static const light = Color(0xFFF6F6F6);
+  static const dark = Color(0xFF272727);
+  static const primaryBackground = Color(0xFFF3F5FF);
+
+  // Background Container Colors
+  static const tightContainer = Color(0xFFF6F6F6);
+  static final dorkContainer = EColors.white.withOpacity(0.1);
+
+  // Button Colors
+  static const buttonPrimary = Color(0xFF4b68ff);
+  static const buttonSecondary = Color(0xFF6C7570);
+  static const buttonDisabled = Color(0xFFC4C4C4);
+
+  // Border Colors
+  static const borderPrimary = Color(0xFFD9D9D9);
+  static const borderSecondary = Color(0xFFE6E6E6);
+
+  // Error and
+  static const error = Color(0xFFD32F2F);
+  static const success = Color(0xFF388E3C);
+  static const warning = Color(0xFFF57C00);
+  static const info = Color(0xFF1976D2);
+
+  // neutral colors
+  static const block = Color(0xFF232323);
+  static const darkerGrey = Color(0xFF4F4F4F);
+  static const darkGrey = Color(0xFF939393);
+  static const grey = Color(0xFFE0E0E0);
+  static const softGrey = Color(0xFFF4F4F4);
+  static const lightGrey = Color(0xFFF9F9F9);
+  static const white = Color(0xFFFFFFFF);
+}
