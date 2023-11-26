0280625cd4f53611623368d65258b03bad592b73
Dev. Smith <husseinshakir81@gmail.com>
2023-11-26 14:17:35 +0300

    feat(setup): setup custom app bar theme

---
+++ lib/utils/theme/custom/appbar_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EAppBarTheme {
+  EAppBarTheme._();
+
+  static const light = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.transparent,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: Colors.black, size: 24),
+    actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+    titleTextStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black),
+  );
+
+  static const dark = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.transparent,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: Colors.black, size: 24),
+    actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+    titleTextStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black),
+  );
+}
