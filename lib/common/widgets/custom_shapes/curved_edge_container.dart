9ecec81a0da789e3398aacaf8c3de74932f2ea76
Dev. Smith <husseinshakir81@gmail.com>
2023-12-07 11:00:17 +0300

    feat(bg): create custom curved edges widget

---
+++ lib/common/widgets/custom_shapes/curved_edge_container.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'curved_shape.dart';
+
+class CurvedEdgeContainer extends StatelessWidget {
+  const CurvedEdgeContainer({super.key, this.child, this.backgroundCoIor = EColors.primary});
+
+  final Widget? child;
+  final Color backgroundCoIor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: ECurvedClipper(),
+      child: Container(
+        color: backgroundCoIor,
+        padding: const EdgeInsets.all(0),
+        child: child,
+      ),
+    );
+  }
+}
